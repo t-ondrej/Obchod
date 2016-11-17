be3a68584f78dfbd002bb439ff10a3e1920add60
t-ondrej <t.ondrej01@gmail.com>
2016-11-17 10:44:17 +0100

    database & Dao updates

---
+++ src/main/java/sk/upjs/ics/obchod/dao/MysqlPouzivatelDao.java
@@ -0,0 +1,48 @@
+
+package sk.upjs.ics.obchod.dao;
+
+import java.util.List;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import sk.upjs.ics.obchod.entity.Pouzivatel;
+import sk.upjs.ics.obchod.entity.Tovar;
+
+
+public class MysqlPouzivatelDao implements PouzivatelDao{
+    
+    private final JdbcTemplate jdbcTemplate;
+    
+    public MysqlPouzivatelDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+   
+  @Override
+    public List<Pouzivatel> dajPouzivatelov() {
+        String sql = "SELECT * FROM Pouzivatel";
+        
+        BeanPropertyRowMapper<Pouzivatel> mapper = BeanPropertyRowMapper.newInstance(Pouzivatel.class);
+        
+        return jdbcTemplate.query(sql, mapper);
+    }
+    
+
+    @Override
+    public void pridajPouzivatela(Pouzivatel pouzivatel) {
+        String sql = "INSERT INTO Tovar VALUES(? ? ? ? ? ? ?)";
+        
+        jdbcTemplate.update(sql, pouzivatel.getId(), pouzivatel.getKosik().getId(), pouzivatel.getPrihlasovacieMeno(), 
+                pouzivatel.getPasswordHash(), pouzivatel.getSol(), pouzivatel.getEmail(), pouzivatel.getPoslednePrihlasenie());
+    }
+
+    @Override
+    public void odstranPouzivatela(Pouzivatel pouzivatel) {
+        String sql = "DELETE FROM Pouzivatel WHERE id = ?";
+        
+        jdbcTemplate.update(sql, pouzivatel.getId());
+    }
+
+    @Override
+    public void aktualizujPouzivatelov() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
